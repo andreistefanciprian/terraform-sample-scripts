ae58013524baa7c764b6629846dcac6a3831862c
“Stefan <andreistefanciprian@gmail.com>
2018-04-30 07:54:15 +0100

    adding region as variable

--- aws/learn_modules/projects/a/providers.tf
+++ aws/learn_modules/projects/a/providers.tf
@@ -4,5 +4,5 @@
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  region     = "eu-west-2"
+  region     = "${var.my_region}"
 }
--- aws/learn_modules/projects/a/variables.tf
+++ aws/learn_modules/projects/a/variables.tf
@@ -1,3 +1,4 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "mypubkey" {}
+variable "my_region" { default = "eu-west-2" }
